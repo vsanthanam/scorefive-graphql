de5b29644dbc7bd05ee97b94d862002037fa017a
Varun Santhanam <talkto@vsanthanam.com>
2026-02-04 13:34:02 +0530

    Document the schema

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,24 +2,25 @@ import pino from 'pino';
 
 import { production } from '@/utils/env';
 
-const logger = production
-    ? pino({
-          level: process.env.LOG_LEVEL ?? 'info',
-          base: null,
-      })
-    : pino(
-          {
-              level: process.env.LOG_LEVEL ?? 'debug',
-              base: null,
-          },
-          pino.transport({
-              target: 'pino-pretty',
-              options: {
-                  colorize: true,
-                  translateTime: 'SYS:standard',
-                  singleLine: true,
-              },
-          }),
-      );
+const productionLogger = {
+    level: process.env.LOG_LEVEL ?? 'info',
+    base: null,
+};
+
+const debugLogger = {
+    level: process.env.LOG_LEVEL ?? 'debug',
+    base: null,
+};
+
+const debugTransport = pino.transport({
+    target: 'pino-pretty',
+    options: {
+        colorize: true,
+        translateTime: 'SYS:standard',
+        singleLine: true,
+    },
+});
+
+const logger = production ? pino(productionLogger) : pino(debugLogger, debugTransport);
 
 export default logger;
